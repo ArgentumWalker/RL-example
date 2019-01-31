ae7b23ef788096ffda6ea1af71847e22821a5609
ArgentumWalker <svidchen-ko@mail.ru>
2019-01-31 03:22:39 +0300

    fixes

--- pycode/main.py
+++ pycode/main.py
@@ -15,6 +15,12 @@ env = gym.make("MsPacman-v0")
 frame = env.reset()
 
 
+class Flatten(nn.Module):
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        return x
+
+
 def to_grayscale(image):
     #Constants from https://en.wikipedia.org/wiki/Grayscale
     return 0.2126 * image[:, :, 0] + 0.7152 * image[:, :, 1] + 0.0722 * image[:, :, 2]
@@ -30,13 +36,26 @@ plt.imshow(preprocesing(frame), cmap="gray")
 
 model = nn.Sequential(
     nn.Conv2d(4, 32, 8, 4),
+    nn.ReLU(),
     nn.Conv2d(32, 64, 4, 2),
+    nn.ReLU(),
     nn.Conv2d(64, 64, 3, 1),
+    nn.ReLU(),
+    Flatten(),
     nn.Linear(3136, 512),
+    nn.ReLU(),
     nn.Linear(512, 5)
 )
 
 target_model = copy.deepcopy(model)
+
+
+def init_weights(layer):
+    if type(layer) == nn.Linear or type(layer) == nn.Conv2d:
+        nn.init.xavier_normal(layer.weight)
+        
+
+model.apply(init_weights)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -53,21 +72,25 @@ epsilon = 0.1
 def select_action(state):
     if random.random() < epsilon:
         return random.randint(0, 4)
-    return model(torch.tensor(state).to(device).unsqueeze(0))[0].max(1)[1].view(1, 1).item()
+    return model(torch.tensor(state).to(device).float().unsqueeze(0))[0].max(0)[1].view(1, 1).item()
 
 
 def fit(batch):
         state, action, reward, next_state, done = batch
+        state = torch.tensor(state).to(device).float()
+        next_state = torch.tensor(next_state).to(device).float()
+        reward = torch.tensor(reward).to(device).float()
+        action = torch.tensor(action).to(device)
 
         target_q = torch.zeros(reward.size()[0]).float().to(device)
         with torch.no_grad():
             #Get predicted by target model Q-function
-            target_q[done] = target_model(next_state)[0].max(1)[0].detach()
+            target_q[done] = target_model(next_state).max(1)[0].detach()
         #Estimate current Q-function
         target_q = reward + target_q * gamma
 
         #Current approximation
-        q = model(state)[0].gather(1, action.unsqueeze(1))
+        q = model(state).gather(1, action.unsqueeze(1))
 
         loss = F.smooth_l1_loss(q, target_q.unsqueeze(1))
 
@@ -103,17 +126,18 @@ class Memory:
 
 
 memory = Memory(10000)
-last_four_frames = deque(4)
+last_four_frames = deque(maxlen=4)
 
 for i in range(4):
-    last_four_frames.append(frame)
+    last_four_frames.append(np.expand_dims(preprocesing(frame), 2))
 
 summary_reward = 0.
+
 actual_state = np.swapaxes(np.concatenate(last_four_frames, axis=2), 2, 0)
 for step in range(1000000):
     action = select_action(actual_state)
-    state, reward, done, _ = env.step()
-    last_four_frames.append(preprocesing(state))
+    state, reward, done, _ = env.step(action)
+    last_four_frames.append(np.expand_dims(preprocesing(state), 2))
 
     summary_reward += reward
 
@@ -121,7 +145,7 @@ for step in range(1000000):
         memory.push((actual_state, action, reward, None, done))
         frame = env.reset()
         for i in range(4):
-            last_four_frames.append(frame)
+            last_four_frames.append(np.expand_dims(preprocesing(frame), 2))
         done = False
         actual_state = np.swapaxes(np.concatenate(last_four_frames, axis=2), 2, 0)
     else:
@@ -129,7 +153,8 @@ for step in range(1000000):
         memory.push((actual_state, action, reward, new_state, done))
         actual_state = new_state
 
-    fit(np.array(zip(*memory.sample(32))))
+    if step > 32:
+        fit(list(zip(*memory.sample(32))))
 
     if step % 10000 == 0:
         target_model = copy.deepcopy(model)
